2fafe1cb134e0ee262dd58d0d3fbf6735e1c839c
Thomas Amar <hello@ebabel.eu>
2018-09-13 03:18:14 +0200

    move code from game to separate chat module

--- client/src/game.js
+++ client/src/game.js
@@ -1,31 +1,8 @@
+import { chatMessage } from './modules/chat-message.js';
+
+// Main game module that co-ordinates all other modules.
 const game = (socket) => {
-  // Multi-player chat.
-  socket.on('chatMessage', (chatMessage) => {
-    const logsList = document.getElementById('logsList');
-    const li = document.createElement('li');
-    li.textContent = chatMessage;
-
-    // Remove old messages.
-    if (logsList.childNodes.length > 5) {
-      while (logsList.childNodes.length > 5) {
-        logsList.removeChild(logsList.firstChild);
-      }
-    }
-
-    // Display latest message.
-    logsList.append(li);
-
-    // Scroll to the latest message.
-    logsList.scrollTop = logsList.scrollHeight;
-  });
-
-  // Emit current player chat message to all players, via server side.
-  document.getElementById('logsForm').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const chatMessage = document.getElementById('logsInput').value;
-    socket.emit('chatMessage', chatMessage);
-    document.getElementById('logsInput').value = '';
-  });
+  chatMessage(socket);
 };
 
 game(socket);
---
+++ client/src/modules/chat-message.js
@@ -0,0 +1,31 @@
+// Multi-player chat.
+const chatMessage = (socket) => {
+  socket.on('chatMessage', (chatMessage) => {
+    const logsList = document.getElementById('logsList');
+    const li = document.createElement('li');
+    li.textContent = chatMessage;
+
+    // Remove old messages.
+    if (logsList.childNodes.length > 5) {
+      while (logsList.childNodes.length > 5) {
+        logsList.removeChild(logsList.firstChild);
+      }
+    }
+
+    // Display latest message.
+    logsList.append(li);
+
+    // Scroll to the latest message.
+    logsList.scrollTop = logsList.scrollHeight;
+  });
+
+  // Emit current player chat message to all players, via server side.
+  document.getElementById('logsForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const chatMessage = document.getElementById('logsInput').value;
+    socket.emit('chatMessage', chatMessage);
+    document.getElementById('logsInput').value = '';
+  });
+};
+
+export { chatMessage };
