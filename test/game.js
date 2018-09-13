606a7e60a953e07eb011484436989def927818e9
Thomas Amar <hello@ebabel.eu>
2018-09-13 03:25:41 +0200

    plan to test

--- test/game.js
+++ test/game.js
@@ -3,10 +3,12 @@ const chai = require('chai');
 const expect = chai.expect;
 const assert = chai.assert;
 
-const game = require('../client/src/game.js');
+// const game = require('../client/src/game.js');
 
 describe('game', () => {
-  it('should return the expected version', () => {
-    console.log(game);
+  it('exists', () => {
+    // checking if test works (always true).
+    assert(true);
+    // todo: read documentation on http://teejayvanslyke.com/how-to-set-up-a-test-runner-for-modern-javascript.html
   });
 });
