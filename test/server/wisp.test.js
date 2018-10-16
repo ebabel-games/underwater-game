de577b6e99ac4c6e3f12402eb7ab344b62315d79
Thomas Amar <hello@ebabel.eu>
2018-10-16 08:03:55 +0200

    Improved unit test.

--- test/server/wisp.test.js
+++ test/server/wisp.test.js
@@ -2,10 +2,26 @@
 
 const Wisp = require('../../server/wisp');
 
-test('Wisp can instantiate a wisp object with traits greater than 0.', () => {
+test('Wisp can instantiate a wisp object with default values when no input is supplied to the constructor.', () => {
   const result = new Wisp();
   expect(result.name).toBe('a wisp');
+  expect(result.strength).toBeGreaterThan(0);
+  expect(result.stamina).toBeGreaterThan(0);
+  expect(result.agility).toBeGreaterThan(0);
+  expect(result.position.length).toBe(3);
+  expect(isNaN(result.position[0])).toBe(false);
+  expect(isNaN(result.position[1])).toBe(false);
+  expect(isNaN(result.position[2])).toBe(false);
+  expect(result.rotation.length).toBe(3);
+  expect(isNaN(result.rotation[0])).toBe(false);
+  expect(isNaN(result.rotation[1])).toBe(false);
+  expect(isNaN(result.rotation[2])).toBe(false);
   expect(result.life).toBeGreaterThan(0);
+  expect(result.attack).toBeGreaterThan(0);
+  expect(result.defence).toBeGreaterThan(0);
+  expect(result.fightMode).toBe(false);
+  expect(result.color).toEqual([0.64, 0.9, 0.7]);
+  expect(result.respawnedLife).toEqual(result.life);
 });
 
 test('Wisp defaults to 20 life when stamina and agility sum are exactly the same value as strength.', () => {
