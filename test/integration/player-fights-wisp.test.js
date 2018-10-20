dbdbb76fca6f1973ebed2aa7bbbfab2916fa8060
Thomas Amar <hello@ebabel.eu>
2018-10-20 16:23:29 +0200

    Improve integration test.

--- test/integration/player-fights-wisp.test.js
+++ test/integration/player-fights-wisp.test.js
@@ -6,18 +6,29 @@ const resolveFight = require('../../server/resolve-fight');
 
 let thomas;
 let mob;
+let log;
 
 beforeEach(() => {
   thomas = new Player({ socketId: 1, name: 'Thomas' });
   mob = new Wisp();
+  log = [];
 });
 
-test('Setup a player and a mob then run a single exchange of fighting.', () => {
-  const result = resolveFight(thomas, mob);
+test('Setup a player and a mob then fight until either of them dies.', () => {
+  log.push(`${thomas.name} life before fight: ${thomas.life} life.`);
+  log.push(`${mob.name} life before fight: ${mob.life} life.`);
 
-  console.log(thomas); /* eslint no-console: 0 */
+  const fightMessages = [];
+  while(thomas.life > 0 && mob.life > 0) {
+    const result = resolveFight(thomas, mob);
+    fightMessages.push(...result.fightMessages);
+  }
 
-  expect(thomas).not.toBe(undefined);
-  expect(mob).not.toBe(undefined);
-  expect(result).not.toBe(undefined);
+  log.push(...fightMessages);
+  log.push(`${thomas.name} life after fight: ${thomas.life} life.`);
+  log.push(`${mob.name} life after fight: ${mob.life} life.`);
+
+  console.log(log); /* eslint no-console: 0 */
+
+  expect(thomas.life <= 0 || mob.life <= 0).toBe(true);
 });
