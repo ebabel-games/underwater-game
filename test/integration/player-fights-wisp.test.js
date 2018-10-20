85a2a5947d0a66e10561bf782e631bc5b994151e
Thomas Amar <hello@ebabel.eu>
2018-10-20 17:57:09 +0200

    Add log4js to log integration tests.

--- test/integration/player-fights-wisp.test.js
+++ test/integration/player-fights-wisp.test.js
@@ -1,32 +1,37 @@
 'strict';
 
+const log4js = require('log4js');
+log4js.configure({
+  appenders: { player_fights_wisp: { type: 'file', filename: 'player_fights_wisp.log' } },
+  categories: { default: { appenders: [ 'player_fights_wisp' ], level: 'all' } }
+});
+
+const logger = log4js.getLogger('player_fights_wisp');
+logger.level = 'all';
+
 const Player = require('../../server/player');
 const Wisp = require('../../server/wisp');
 const resolveFight = require('../../server/resolve-fight');
 
 let thomas;
 let mob;
-let log;
 
 beforeEach(() => {
   thomas = new Player({ socketId: 1, name: 'Thomas' });
   mob = new Wisp();
-  log = [];
 });
 
 test('Fight 1 - Setup a player and a mob then fight until either of them dies.', () => {
-  log.push(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  logger.info(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
 
   const fightMessages = [];
   while(thomas.life > 0 && mob.life > 0) {
     const result = resolveFight(thomas, mob);
     fightMessages.push(...result.fightMessages);
   }
-  log.push(...fightMessages);
+  fightMessages.map((message) => logger.info(message));
 
-  log.push(`Fight result: ${fightMessages.length / 2} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
-
-  console.log(log); /* eslint no-console: 0 */
+  logger.info(`Fight result: ${fightMessages.length} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
 
   expect(thomas.life <= 0 || mob.life <= 0).toBe(true);
 });
@@ -34,18 +39,16 @@ test('Fight 1 - Setup a player and a mob then fight until either of them dies.',
 test('Fight 2 - Setup a player (with 500 life) and a mob then fight until either of them dies.', () => {
   thomas.life = 500;
 
-  log.push(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  logger.info(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
 
   const fightMessages = [];
   while(thomas.life > 0 && mob.life > 0) {
     const result = resolveFight(thomas, mob);
     fightMessages.push(...result.fightMessages);
   }
-  log.push(...fightMessages);
+  fightMessages.map((message) => logger.info(message));
 
-  log.push(`Fight result: ${fightMessages.length / 2} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
-
-  console.log(log); /* eslint no-console: 0 */
+  logger.info(`Fight result: ${fightMessages.length} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
 
   expect(thomas.life <= 0 || mob.life <= 0).toBe(true);
 });
@@ -53,18 +56,16 @@ test('Fight 2 - Setup a player (with 500 life) and a mob then fight until either
 test('Fight 3 - Setup a player (with 400 life) and a mob then fight until either of them dies.', () => {
   thomas.life = 400;
 
-  log.push(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  logger.info(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
 
   const fightMessages = [];
   while(thomas.life > 0 && mob.life > 0) {
     const result = resolveFight(thomas, mob);
     fightMessages.push(...result.fightMessages);
   }
-  log.push(...fightMessages);
+  fightMessages.map((message) => logger.info(message));
 
-  log.push(`Fight result: ${fightMessages.length / 2} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
-
-  console.log(log); /* eslint no-console: 0 */
+  logger.info(`Fight result: ${fightMessages.length} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
 
   expect(thomas.life <= 0 || mob.life <= 0).toBe(true);
 });
@@ -72,18 +73,16 @@ test('Fight 3 - Setup a player (with 400 life) and a mob then fight until either
 test('Fight 4 - Setup a player (with 300 life) and a mob then fight until either of them dies.', () => {
   thomas.life = 300;
 
-  log.push(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  logger.info(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
 
   const fightMessages = [];
   while(thomas.life > 0 && mob.life > 0) {
     const result = resolveFight(thomas, mob);
     fightMessages.push(...result.fightMessages);
   }
-  log.push(...fightMessages);
+  fightMessages.map((message) => logger.info(message));
 
-  log.push(`Fight result: ${fightMessages.length / 2} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
-
-  console.log(log); /* eslint no-console: 0 */
+  logger.info(`Fight result: ${fightMessages.length} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
 
   expect(thomas.life <= 0 || mob.life <= 0).toBe(true);
 });
@@ -91,18 +90,16 @@ test('Fight 4 - Setup a player (with 300 life) and a mob then fight until either
 test('Fight 5 - Setup a player (with 200 life) and a mob then fight until either of them dies.', () => {
   thomas.life = 200;
 
-  log.push(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  logger.info(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
 
   const fightMessages = [];
   while(thomas.life > 0 && mob.life > 0) {
     const result = resolveFight(thomas, mob);
     fightMessages.push(...result.fightMessages);
   }
-  log.push(...fightMessages);
+  fightMessages.map((message) => logger.info(message));
 
-  log.push(`Fight result: ${fightMessages.length / 2} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
-
-  console.log(log); /* eslint no-console: 0 */
+  logger.info(`Fight result: ${fightMessages.length} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
 
   expect(thomas.life <= 0 || mob.life <= 0).toBe(true);
 });
@@ -110,18 +107,16 @@ test('Fight 5 - Setup a player (with 200 life) and a mob then fight until either
 test('Round 6 - Setup a player (with 100 life) and a mob then fight until either of them dies.', () => {
   thomas.life = 100;
 
-  log.push(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  logger.info(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
 
   const fightMessages = [];
   while(thomas.life > 0 && mob.life > 0) {
     const result = resolveFight(thomas, mob);
     fightMessages.push(...result.fightMessages);
   }
-  log.push(...fightMessages);
+  fightMessages.map((message) => logger.info(message));
 
-  log.push(`Fight result: ${fightMessages.length / 2} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
-
-  console.log(log); /* eslint no-console: 0 */
+  logger.info(`Fight result: ${fightMessages.length} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
 
   expect(thomas.life <= 0 || mob.life <= 0).toBe(true);
 });
