05327d2fe0b94de8f89d2451451b75771e59db0d
Thomas Amar <hello@ebabel.eu>
2018-10-21 09:32:27 +0200

    Single line logging for the summary of fights.

--- test/integration/player-fights-wisp.test.js
+++ test/integration/player-fights-wisp.test.js
@@ -1,13 +1,31 @@
 'strict';
 
 const log4js = require('log4js');
+
 log4js.configure({
-  appenders: { player_fights_wisp: { type: 'file', filename: 'player_fights_wisp.log' } },
-  categories: { default: { appenders: [ 'player_fights_wisp' ], level: 'all' } }
+  appenders: {
+    summary: {
+      type: 'file',
+      filename: 'player-fights-wisp.log',
+    },
+    full: {
+      type: 'file',
+      filename: 'player-fights-wisp-full.log',
+    },
+  },
+  categories: {
+    default: {
+      level: 'all',
+      appenders: [ 'summary' ],
+    },
+  },
 });
 
-const logger = log4js.getLogger('player_fights_wisp');
-logger.level = 'all';
+const summaryLogger = log4js.getLogger('summary');
+summaryLogger.level = 'all';
+
+const fullLogger = log4js.getLogger('full');
+fullLogger.level = 'all';
 
 const Player = require('../../server/player');
 const Wisp = require('../../server/wisp');
@@ -16,22 +34,33 @@ const resolveFight = require('../../server/resolve-fight');
 let thomas;
 let mob;
 
+const logMessage1 = (message) => `Fight summary: ${message.fightRounds} rounds, ${message.playerName} started with ${message.playerStartLife} life and ended with ${message.playerEndLife} life, ${message.mobName} started with ${message.mobStartLife} life and ended with ${message.mobEndLife}.`;
+const logMessage2 = (message) => `Fight summary: ${message.fightRounds} rounds, ${message.playerName} started with ${message.playerStartLife} life and ended with ${message.playerEndLife} life, fought ${message.mobsFought} ${message.mobName}.`;
+const logMessage3 = (message) => `Fight summary: ${message.fightRounds} rounds, ${message.playerName} started with ${message.playerStartLife} life and ended with ${message.playerEndLife} life, first ${message.mob1Name} started with ${message.mob1StartLife} life and ended with ${message.mob1EndLife}, second ${message.mob2Name} started with ${message.mob2StartLife} life and ended with ${message.mob2EndLife}.`;
+
 beforeEach(() => {
   thomas = new Player({ socketId: 1, name: 'Thomas' });
   mob = new Wisp();
 });
 
 test('Fight 1 - Setup a player and a mob then fight until either of them dies.', () => {
-  logger.info(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  const message = {
+    playerName: thomas.name,
+    playerStartLife: thomas.life,
+    mobName: mob.name,
+    mobStartLife: mob.life,
+    fightRounds: 0,
+  };
 
-  const fightMessages = [];
   while(thomas.life > 0 && mob.life > 0) {
     const result = resolveFight(thomas, mob);
-    fightMessages.push(...result.fightMessages);
+    message.fightRounds += result.fightMessages.length;
   }
-  fightMessages.map((message) => logger.info(message));
 
-  logger.info(`Fight result: ${fightMessages.length} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  message.playerEndLife = thomas.life;
+  message.mobEndLife = mob.life;
+
+  summaryLogger.info(logMessage1(message));
 
   expect(thomas.life <= 0 || mob.life <= 0).toBe(true);
 });
@@ -39,16 +68,23 @@ test('Fight 1 - Setup a player and a mob then fight until either of them dies.',
 test('Fight 2 - Setup a player (with 500 life) and a mob then fight until either of them dies.', () => {
   thomas.life = 500;
 
-  logger.info(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  const message = {
+    playerName: thomas.name,
+    playerStartLife: thomas.life,
+    mobName: mob.name,
+    mobStartLife: mob.life,
+    fightRounds: 0,
+  };
 
-  const fightMessages = [];
   while(thomas.life > 0 && mob.life > 0) {
     const result = resolveFight(thomas, mob);
-    fightMessages.push(...result.fightMessages);
+    message.fightRounds += result.fightMessages.length;
   }
-  fightMessages.map((message) => logger.info(message));
 
-  logger.info(`Fight result: ${fightMessages.length} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  message.playerEndLife = thomas.life;
+  message.mobEndLife = mob.life;
+
+  summaryLogger.info(logMessage1(message));
 
   expect(thomas.life <= 0 || mob.life <= 0).toBe(true);
 });
@@ -56,16 +92,23 @@ test('Fight 2 - Setup a player (with 500 life) and a mob then fight until either
 test('Fight 3 - Setup a player (with 400 life) and a mob then fight until either of them dies.', () => {
   thomas.life = 400;
 
-  logger.info(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  const message = {
+    playerName: thomas.name,
+    playerStartLife: thomas.life,
+    mobName: mob.name,
+    mobStartLife: mob.life,
+    fightRounds: 0,
+  };
 
-  const fightMessages = [];
   while(thomas.life > 0 && mob.life > 0) {
     const result = resolveFight(thomas, mob);
-    fightMessages.push(...result.fightMessages);
+    message.fightRounds += result.fightMessages.length;
   }
-  fightMessages.map((message) => logger.info(message));
 
-  logger.info(`Fight result: ${fightMessages.length} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  message.playerEndLife = thomas.life;
+  message.mobEndLife = mob.life;
+
+  summaryLogger.info(logMessage1(message));
 
   expect(thomas.life <= 0 || mob.life <= 0).toBe(true);
 });
@@ -73,16 +116,23 @@ test('Fight 3 - Setup a player (with 400 life) and a mob then fight until either
 test('Fight 4 - Setup a player (with 300 life) and a mob then fight until either of them dies.', () => {
   thomas.life = 300;
 
-  logger.info(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  const message = {
+    playerName: thomas.name,
+    playerStartLife: thomas.life,
+    mobName: mob.name,
+    mobStartLife: mob.life,
+    fightRounds: 0,
+  };
 
-  const fightMessages = [];
   while(thomas.life > 0 && mob.life > 0) {
     const result = resolveFight(thomas, mob);
-    fightMessages.push(...result.fightMessages);
+    message.fightRounds += result.fightMessages.length;
   }
-  fightMessages.map((message) => logger.info(message));
 
-  logger.info(`Fight result: ${fightMessages.length} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  message.playerEndLife = thomas.life;
+  message.mobEndLife = mob.life;
+
+  summaryLogger.info(logMessage1(message));
 
   expect(thomas.life <= 0 || mob.life <= 0).toBe(true);
 });
@@ -90,16 +140,23 @@ test('Fight 4 - Setup a player (with 300 life) and a mob then fight until either
 test('Fight 5 - Setup a player (with 200 life) and a mob then fight until either of them dies.', () => {
   thomas.life = 200;
 
-  logger.info(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  const message = {
+    playerName: thomas.name,
+    playerStartLife: thomas.life,
+    mobName: mob.name,
+    mobStartLife: mob.life,
+    fightRounds: 0,
+  };
 
-  const fightMessages = [];
   while(thomas.life > 0 && mob.life > 0) {
     const result = resolveFight(thomas, mob);
-    fightMessages.push(...result.fightMessages);
+    message.fightRounds += result.fightMessages.length;
   }
-  fightMessages.map((message) => logger.info(message));
 
-  logger.info(`Fight result: ${fightMessages.length} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  message.playerEndLife = thomas.life;
+  message.mobEndLife = mob.life;
+
+  summaryLogger.info(logMessage1(message));
 
   expect(thomas.life <= 0 || mob.life <= 0).toBe(true);
 });
@@ -107,65 +164,87 @@ test('Fight 5 - Setup a player (with 200 life) and a mob then fight until either
 test('Round 6 - Setup a player (with 100 life) and a mob then fight until either of them dies.', () => {
   thomas.life = 100;
 
-  logger.info(`Fight start: ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  const message = {
+    playerName: thomas.name,
+    playerStartLife: thomas.life,
+    mobName: mob.name,
+    mobStartLife: mob.life,
+    fightRounds: 0,
+  };
 
-  const fightMessages = [];
   while(thomas.life > 0 && mob.life > 0) {
     const result = resolveFight(thomas, mob);
-    fightMessages.push(...result.fightMessages);
+    message.fightRounds += result.fightMessages.length;
   }
-  fightMessages.map((message) => logger.info(message));
 
-  logger.info(`Fight result: ${fightMessages.length} rounds, ${thomas.name} ${thomas.life} life, ${mob.name} ${mob.life} life.`);
+  message.playerEndLife = thomas.life;
+  message.mobEndLife = mob.life;
+
+  summaryLogger.info(logMessage1(message));
 
   expect(thomas.life <= 0 || mob.life <= 0).toBe(true);
 });
 
-test('Player fights multiple wisps, one after the other, until the player dies', () => {
-  let mobsFought = 1;
+test('Player fights multiple wisps, one after the other, until the player dies.', () => {
+  const message = {
+    playerName: thomas.name,
+    playerStartLife: thomas.life,
+    mobName: mob.name,
+    fightRounds: 0,
+    mobsFought: 1,
+  };
 
-  logger.info(`Fight start: ${thomas.name} ${thomas.life} life.`);
-
-  const fightMessages = [];
   while(thomas.life > 0) {
     const result = resolveFight(thomas, mob);
-    fightMessages.push(...result.fightMessages);
+    message.fightRounds += result.fightMessages.length;
 
     if (mob.life <= 0) {
       mob = new Wisp();
-      mobsFought += 1;
+      message.mobsFought += 1;
     }
   }
-  fightMessages.map((message) => logger.info(message));
 
-  logger.info(`Fight result: ${fightMessages.length} rounds, ${thomas.name} ${thomas.life} life, ${mobsFought} mobs fought.`);
+  message.playerEndLife = thomas.life;
+
+  summaryLogger.info(logMessage2(message));
 
   expect(thomas.life <= 0).toBe(true);
 });
 
-test('Player fights 2 wisps at the same time.', () => {
+test('Player fights 2 wisps at the same time until either the player or both wisps are dead.', () => {
   const mob1 = new Wisp();
   const mob2 = new Wisp();
 
-  logger.info(`Fight start: ${thomas.name} ${thomas.life} life, 2 mobs ${mob1.life} life and ${mob2.life} life.`);
+  const message = {
+    playerName: thomas.name,
+    playerStartLife: thomas.life,
+    mob1Name: mob1.name,
+    mob1StartLife: mob1.life,
+    mob2Name: mob2.name,
+    mob2StartLife: mob2.life,
+    fightRounds: 0,
+    mobsFought: 2,
+  };
 
-  const fightMessages = [];
   while(thomas.life > 0) {
     const result1 = resolveFight(thomas, mob1);
-    fightMessages.push(...result1.fightMessages);
+    message.fightRounds += result1.fightMessages.length;
 
     // Only keep fighting if player is still alive.
     if (thomas.life > 0) {
       const result2 = resolveFight(thomas, mob2);
-      fightMessages.push(...result2.fightMessages);
+      message.fightRounds += result2.fightMessages.length;
     }
 
     // Stop fightings if both mob1 and mob2 are dead.
     if (mob1.life <= 0 && mob2.life <= 0) break;
   }
-  fightMessages.map((message) => logger.info(message));
 
-  logger.info(`Fight result: ${fightMessages.length} rounds, ${thomas.name} ${thomas.life} life, 2 mobs fought, ${mob1.life} life and ${mob2.life} life.`);
+  message.playerEndLife = thomas.life;
+  message.mob1EndLife = mob1.life;
+  message.mob2EndLife = mob2.life;
+
+  summaryLogger.info(logMessage3(message));
 
   expect(thomas.life <= 0 || (mob1.life <= 0 && mob2.life <= 0)).toBe(true);
 });
